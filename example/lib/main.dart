99392b85eb2bc4454ab47187e41d18c3b1c50494
Purna Kalyan Shakya <38076182+landxcape@users.noreply.github.com>
2025-04-20 16:48:37 +0545

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,14 +1,16 @@
 import 'dart:convert';
-import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_usb_printer/flutter_usb_printer.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
-  _MyAppState createState() => new _MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
@@ -60,25 +62,23 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('USB PRINTER'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('USB PRINTER'),
           actions: <Widget>[
-            new IconButton(
-                icon: new Icon(Icons.refresh),
+            IconButton(icon: Icon(Icons.refresh),
                 onPressed: () => _getDevicelist()),
             connected == true
-                ? new IconButton(
-                    icon: new Icon(Icons.print),
+                ? IconButton(
+                  icon: Icon(Icons.print),
                     onPressed: () {
                       _print();
                     })
-                : new Container(),
+                : Container(),
           ],
         ),
-        body: devices.length > 0
-            ? new ListView(
+        body: devices.isNotEmpty ? ListView(
                 scrollDirection: Axis.vertical,
                 children: _buildList(devices),
               )
@@ -89,16 +89,17 @@ class _MyAppState extends State<MyApp> {
 
   List<Widget> _buildList(List<Map<String, dynamic>> devices) {
     return devices
-        .map((device) => new ListTile(
+        .map(
+          (device) => ListTile(
               onTap: () {
                 _connect(int.parse(device['vendorId']),
                     int.parse(device['productId']));
               },
-              leading: new Icon(Icons.usb),
-              title: new Text(
+            leading: Icon(Icons.usb),
+            title: Text(
                   device['manufacturer'] + " " + device['productName']),
               subtitle:
-                  new Text(device['vendorId'] + " " + device['productId']),
+                  Text(device['vendorId'] + " " + device['productId']),
             ))
         .toList();
   }
